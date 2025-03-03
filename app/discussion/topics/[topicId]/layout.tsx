32f00891938556048c8fc530fe33be3e4d678347
ftanekha <farai.tanekha@gmail.com>
2025-03-03 09:03:15 +0000

    - use useRouter for programatic backward navigation to Topics path

--- app/discussion/topics/[topicId]/layout.tsx
+++ app/discussion/topics/[topicId]/layout.tsx
@@ -1,14 +1,19 @@
+'use client'
+
 import React from 'react'
 import Button from '../../../../components/button/Button'
+import { useRouter } from 'next/navigation'
 
 function Layout(
     {children}:{children: React.ReactNode}
 ){
+    const router = useRouter()
+    
     return (
         <div>
             {children}
             <footer>
-                <Button href="/discussion/topics" label="Back To All Topics" background="bg-gray-900 text-gray-200"/>
+                <Button label="Back To All Topics" background="bg-gray-900 text-gray-200" onClick={router.back}/>
             </footer>
         </div>
     )
